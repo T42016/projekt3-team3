4a20e6d365fc447e8162546d41b9815b2ebfaaf9
runeanderberg <rune.anderberg@lund.ntig.se>
2016-09-19 09:35:21 +0200

    added DrawBoardTests

--- MemoryTest/MemoryUnitTest/DrawBoardTests.cs
+++ MemoryTest/MemoryUnitTest/DrawBoardTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MemoryLogic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MemoryUnitTest
@@ -6,7 +7,21 @@ namespace MemoryUnitTest
     [TestClass]
     public class DrawBoardTests
     {
-        
-            
+        [TestInitialize]
+        public void Setup()
+        {
+            MemoryGame _underTest = new MemoryGame(2,2);
+        }
+
+        [TestMethod]
+        public void DrawBoardShouldDoSomething()
+        {
+            // Arrange via Setup
+
+            // Act
+
+            // Assert
+
+        }
     }
 }
